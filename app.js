2a70abf1c842206aecc838d2414cd2f5bbb93705
RileyCowan <rileycowan@u.boisestate.edu>
2025-12-05 19:48:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,35 @@
-const express = require('express');
-const path = require('path');
-const { dbMiddleware} = require('./bin/db');
+// app.js
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const expressLayouts = require("express-ejs-layouts");
 
-
-const indexRouter = require('./routes/index');
-//add more handlers here
+const indexRouter = require("./routes/index");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// View engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("layout", "layout"); // express-ejs-layouts default layout file name
+app.use(expressLayouts);
 
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(dbMiddleware);
-app.use('/', indexRouter);
-//add more routes here
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Routes
+app.use("/", indexRouter);
+
+// 404
+app.use((req, res) => {
+  res.status(404).render("404", { url: req.originalUrl });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
-
-module.exports = app;
